b1512a66f418119cc29f86cabd11337d0ba833a9
Andre Duarte <aguimaraesduarte@dons.usfca.edu>
2017-06-02 11:30:06 -0700

    add GA script

--- server.py
+++ server.py
@@ -32,6 +32,17 @@ def make_html():
         <meta charset="UTF-8">
         <link rel=stylesheet type=text/css href="static/main.css">
         <title>Nostalgia -> {song} - {artist}</title>
+        <!-- START GOOGLE ANALYTICS -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-100396571-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+        <!-- END GOOGLE ANALYTICS -->
     </head>
     <body>
     {body}
