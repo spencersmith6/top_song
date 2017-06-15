30a7e644f4c9b815994c9599228dbf8ca5a85c92
Ubuntu <ubuntu@ip-172-31-8-220.us-west-2.compute.internal>
2017-06-15 18:37:02 +0000

    v2

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
-def get_top_song(dob):
+def get_top_song(dob, hot100):
     year, month, day = dob
     days = hot100[year][month].keys()
     day_key = days[np.argmin(abs(np.array(days) - day))]
@@ -34,13 +34,14 @@ def make_html():
         <title>Nostalgia -> {song} - {artist}</title>
         <!-- START GOOGLE ANALYTICS -->
         <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
+          (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          }})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
           ga('create', 'UA-100396571-1', 'auto');
           ga('send', 'pageview');
+
         </script>
         <!-- END GOOGLE ANALYTICS -->
     </head>
@@ -61,11 +62,15 @@ def index():
 
 @app.route('/process', methods = ['POST'])
 def results():
+
+    with open('/home/ubuntu/nostalgia/hot100_dict.p', 'r') as f:
+        hot100 = pickle.load(f)
+
     y = int(request.form['dob-year'])
     m = int(request.form['dob-month'])
     d = int(request.form['dob-day'])
 
-    top_songs = get_top_song([y,m,d])
+    top_songs = get_top_song([y,m,d], hot100)
     song = top_songs[1][0]
     artist = top_songs[1][1]
 
@@ -77,16 +82,14 @@ def results():
     <p>On <b>{d}-{m}-{y}</b>, the top song was <b>{song}</b> by <b>{artist}</b>.<br>
     Check it out below!</p>
     <div class="video-container">
-        <iframe src="http://www.youtube.com/embed/{id}" allowfullscreen></iframe>
+        <iframe src="http://www.youtube.com/embed/{id}" allowfullscreen onmouseover="ga('send', 'event', 'video', 'hover', 'msan695', 10);" onClick="ga('send', 'event', 'video', 'play', 'msan697', 10);"></iframe>
     </div>
-    <p><button onclick="javascript:history.back()">Go back</button></p>
+    <p><button onclick="javascript:history.back(); ga('send', 'event', 'button', 'back', 'masn697', 10);">Go back</button></p>
     """.format(d=d, m=m, y=y, song=song, artist=artist, id=youtube_id)
 
     return html.format(song=song, artist=artist, body=body)
 
 
 if __name__ == '__main__':
-    with open('hot100_dict.p', 'r') as f:
-        hot100 = pickle.load(f)
-
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+    #app.run(host='0.0.0.0', port=5000)
